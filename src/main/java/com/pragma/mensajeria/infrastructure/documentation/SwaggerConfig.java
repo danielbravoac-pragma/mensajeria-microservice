4b4abe3a1a499a6550d96ed67fa53ad442f06539
Daniel Bravo <daniel.bravo@pragma.com.co>
2025-08-11 10:18:34 -0500

    feat: obs ok

---
+++ src/main/java/com/pragma/mensajeria/infrastructure/documentation/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.pragma.mensajeria.infrastructure.documentation;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Mensajes")
+                        .version("1.0")
+                        .description("Documentación de la API para envío de notificaciones.")
+                        .contact(new Contact()
+                                .name("Equipo de Desarrollo")
+                                .email("soporte@empresa.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
--- src/main/java/com/pragma/mensajeria/infrastructure/input/rest/MessageController.java
+++ src/main/java/com/pragma/mensajeria/infrastructure/input/rest/MessageController.java
@@ -1,6 +1,12 @@
 package com.pragma.mensajeria.infrastructure.input.rest;
 
 import com.pragma.mensajeria.application.handler.IMessageHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/messages")
+@Tag(name = "Message", description = "Gestión de mensajes.")
 public class MessageController {
 
     private final IMessageHandler messageHandler;
 
     @PostMapping("/send-pin")
+    @Operation(
+            summary = "Enviar Mensaje",
+            security = @SecurityRequirement(name = "bearerAuth"),
+            description = "Enviar Mensaje con cuerpo .",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Enviado Correctamente",
+                            content = @Content(schema = @Schema(implementation = Void.class))),
+                    @ApiResponse(responseCode = "500", description = "Error Interno")
+            }
+    )
     public ResponseEntity<Void> sendPin(@RequestParam(name = "phoneNumber") String phoneNumber,
                                         @RequestParam(name = "pin") String pin) {
         messageHandler.sendMessage(phoneNumber, pin);
